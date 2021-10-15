3379ef6f50f127702cb053cb47bcedd7896882ef
Prod <62126042+Cg-Prodigy@users.noreply.github.com>
2021-10-15 11:56:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,9 @@ from kaki.app import App
 from kivymd.app import MDApp
 
 class HotReload(App,MDApp):
-    CLASSES={
-        'EntryPoint':'app.main_ui'
-    }
-    KV_FILES=[
-        'app/kivy_lang.kv'
-    ]
-    AUTORELOADER_PATHS=[
-        ('.',{'recursive':True})
-    ]
-
+    CLASSES={'EntryPoint':'app.main_ui'}
+    KV_FILES=['app/kivy_lang.kv']
+    AUTORELOADER_PATHS=[('.',{'recursive':True})]
     def build_app(self):
         return Factory.EntryPoint()
 
