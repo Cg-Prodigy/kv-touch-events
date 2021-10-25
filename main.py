1918504280ee4b9834757684d83d3de50e105553
Prod <62126042+Cg-Prodigy@users.noreply.github.com>
2021-10-25 19:48:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.core.window import Window
 from kivy.factory import Factory
 from kaki.app import App
 from kivymd.app import MDApp
-window.size=(360,640)
+Window.size=(360,640)
 class HotReload(App,MDApp):
     CLASSES={'EntryPoint':'app.main_ui'}
     KV_FILES=['app/kivy_lang.kv']
